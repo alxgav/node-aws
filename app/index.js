abab9c10f38bd764a74a6b63bef608158de01474
alxgav <alxgav@gmail.com>
2023-12-03 20:19:37 +0100

    change server

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.APP_PORT ?? 3000
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('test AWS')
 })
 
 app.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}`))
